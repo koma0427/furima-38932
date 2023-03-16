1190c7866fa9674c14973d229731ffd9767fd9ac
koma0427 <komatsur0427@gmail.com>
2023-03-16 14:16:36 +0900

    Revert "Revert "テストコード入力""
    This reverts commit 01c318f7780689015e5be68c0b74339d4b4adbb9.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,36 @@
 class ItemsController < ApplicationController
+
+  before_action :move_to_index, except: [:index, :show]
+
   def index
-    render 'items/index'
+    @items = Item.order("created_at DESC")
   end
-end
+
+
+  def new
+    @item = Item.new
+  end
+
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image,:name,:product_description,:category_id,:status_id,:delivery_burden_id,:prefecture_id,:delivery_time_id,:selling_price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+  end
+  
