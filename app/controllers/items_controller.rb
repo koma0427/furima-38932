55770f699a0e000dc0a462355887a0434db0c35a
koma0427 <komatsur0427@gmail.com>
2023-03-18 17:31:27 +0900

    商品詳細表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,17 @@
 class ItemsController < ApplicationController
-
-  before_action :authenticate_user!, except: [:index]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index,:show]
+  before_action :set_item, only: [:show]
   
   def index
     @items = Item.all.order(created_at: :desc) 
   end
 
+  def show
+  end
 
   def new
     @item = Item.new
   end
-
 
   def create
     @item = Item.new(item_params)
@@ -28,10 +28,7 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image,:name,:product_description,:category_id,:status_id,:delivery_burden_id,:prefecture_id,:delivery_time_id,:selling_price).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
+  def set_item
+    @item = Item.find(params[:id])
   end
-  end
-  
+end
