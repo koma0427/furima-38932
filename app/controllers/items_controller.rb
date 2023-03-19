19fb2e448711b480fca723bd54f1be4115805173
koma0427 <komatsur0427@gmail.com>
2023-03-19 18:56:06 +0900

    商品情報編集機能修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
-  before_action :set_item, only: [:show]
+  before_action :set_item, only: [:show,:edit,:update]
   
   def index
     @items = Item.all.order(created_at: :desc) 
@@ -21,6 +21,23 @@ class ItemsController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    if @item.user_id == current_user.id 
+    else
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @item.update(item_params)
+    if @item.valid?
+      redirect_to item_path(item_params)
+    else
+      render 'edit'
+    end
+  end
+
 
   private
 
